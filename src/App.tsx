0e5ef9e2c0f7455c7f1dd5c5434dcad020bf9672
VinayKumar <vinnu3005@gmail.com>
2022-04-10 00:21:59 +0530

    updatde page styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Box } from '@mui/system';
-import { AppBar } from '@mui/material';
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import Website from './website';
 
 function App() {
   return (
     <div className="App">
-      <AppBar/>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            S-Chatt
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
       <Box minHeight={100} textAlign="center">
-        This is Chat BOX
+        <Website/>
       </Box>
     </div>
   );
---
+++ src/website/Website.tsx
@@ -0,0 +1,28 @@
+import { Box, Button, Grid, Stack, Typography } from "@mui/material";
+
+const Website = ()=>{
+    return (
+    <Box justifyContent={"center"} textAlign="center" sx={{
+        verticalAlign:'center'
+    }}>
+        <Grid container spacing={2}>
+            <Grid item lg={12} md={12}>
+                <Typography fontWeight={"bold"} variant="h4">SChatt</Typography>
+            </Grid>
+            <Grid item lg={12} md={12}>
+                <Typography fontWeight={"bold"} variant="body1">Simple socila Media App</Typography>
+            </Grid>
+            <Grid item lg={12} md={12}>
+               <Stack>
+                   <Button variant="contained">
+                       Signup
+                   </Button>
+                  
+               </Stack>
+            </Grid>
+        </Grid>
+    </Box>
+    );
+    
+}
+export default Website;
