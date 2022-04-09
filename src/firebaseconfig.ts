19b3482ba3e367c95b48fcbed716ffbc19c434a8
VinayKumar <vinnu3005@gmail.com>
2022-04-09 19:44:19 +0530

    added material Ui and firebase config

---
+++ src/firebaseconfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBz2FTDYia1g3d9e2d6ryJzZesNeZT07tk",
+  authDomain: "schatt.firebaseapp.com",
+  projectId: "schatt",
+  storageBucket: "schatt.appspot.com",
+  messagingSenderId: "746385083543",
+  appId: "1:746385083543:web:e201e5ccd8be840918add2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
